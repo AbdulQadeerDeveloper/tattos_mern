fefef892d806cc1864cdfe4df3118712e33bee91
DeveloperQadeer <AbdulQadeerDeveloper>
2024-04-30 11:06:34 +0000

    Develop CRUD operations for user management via new API endpoints

--- server/app/routes/authRoute/loginRoute.js
+++ server/app/routes/authRoute/loginRoute.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+// Develop CRUD operations for user management via new API endpoints
 const loginController = require('../../controllers/authController/loginController');
 
 router.post('/api/login', loginController.login);
